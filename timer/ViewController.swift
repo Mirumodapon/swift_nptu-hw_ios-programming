4b0ba9b6201835170c773373c8b58558b5762209
mysper <anthony.a3203@gmail.com>
2021-03-12 12:01:15 +0800

    [IOS] ios timer

---
+++ timer/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  timer
+//
+//  Created by students on 2021/3/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func reset(_ sender: Any) {
+    }
+    @IBAction func pasue(_ sender: Any) {
+    }
+    @IBAction func start(_ sender: Any) {
+    }
+    @IBOutlet weak var clock: UILabel!
+}
+
