2800687046f3f3a5255aedb0cdfc9edca5338606
students <students@Mac19.local>
2021-03-19 11:03:41 +0800

    update

--- TableView/ViewController.swift
+++ TableView/ViewController.swift
@@ -7,7 +7,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    var cellArr = ["Apple","Banana","Cat","Dog"];
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cellArr.count;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "MyCell");
+        cell.textLabel?.text = cellArr[indexPath.row];
+        return cell;
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
