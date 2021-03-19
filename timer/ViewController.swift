b7532f2aa83bf197324bc9598171b4b07d6f91fd
students <students@Mac19.local>
2021-03-19 10:18:30 +0800

    update

--- timer/ViewController.swift
+++ timer/ViewController.swift
@@ -8,17 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setTime(time: 0);
+        timerunning = false;
     }
     @IBAction func reset(_ sender: Any) {
+        setTime(time: 0);
     }
     @IBAction func pasue(_ sender: Any) {
+        myTime.invalidate();
+        timerunning = false;
     }
     @IBAction func start(_ sender: Any) {
+        if timerunning {
+            return;
+        } else {
+            myTime = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timeAddOne), userInfo: nil, repeats: true)
+            myTime.fire();
+            timerunning = true;
+        }
     }
     @IBOutlet weak var clock: UILabel!
+    
+    @objc func timeAddOne() {
+        setTime(time: second+1);
+    }
+    func setTime(time: Int) {
+        second = time;
+        clock.text = String(second);
+    }
+    var second: Int = 0;
+    var myTime = Timer();
+    var timerunning: Bool = false;
 }
 
